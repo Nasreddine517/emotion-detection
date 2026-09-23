5eeb5c3a001c4187f0598d865d27556103c3bc0d
Nasreddine517 <nasreddineelmekkaoui@gmail.com>
2026-09-23 17:23:29 -0400

    Final version: error handling and pylint 10/10

---
+++ server.py
@@ -0,0 +1,42 @@
+"""Emotion detector application using Flask"""
+
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+
+@app.route("/emotionDetector")
+def emot_detector():
+    """This function receives text from the request, calls emotion_detector()
+    and returns a formatted response as a string."""
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+
+    if response['dominant_emotion'] is None:
+        return "Invalid text! Please try again!"
+
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+
+    return (
+        f"For the given statement, the system response is "
+        f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, "
+        f"'joy': {joy}, and 'sadness': {sadness}. "
+        f"The dominant emotion is {dominant_emotion}."
+    )
+
+
+@app.route("/")
+def render_index_page():
+    """This function initiates the rendering of the main application
+    page over the Flask channel."""
+    return render_template('index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
